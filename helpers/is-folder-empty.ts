c4d9f03c2b3fcc2de2ceb2ea1c5175445e4b10c1
ducanh-ho2296 <ducanh-ho2296@users.noreply.github.com>
2024-05-25 00:36:52 +0000

    Merge latest tag from upstream

--- helpers/is-folder-empty.ts
+++ helpers/is-folder-empty.ts
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2024 Deutsche Telekom AG, LlamaIndex, Vercel, Inc.
-//
-// SPDX-License-Identifier: MIT
-
 /* eslint-disable import/no-extraneous-dependencies */
 import fs from "fs";
 import path from "path";
